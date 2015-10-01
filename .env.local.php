1b384f176c6cc24c2f89d548c27d37f18a3e823e
Dennis Stolmeijer <dstollie@hotmail.com>
2015-10-01 23:58:28 +0200

    modify the environment local configurations for my own machine. Later on this should be ignored

---
+++ .env.local.php
@@ -0,0 +1,12 @@
+<?php
+/*----------------------------------------------------*/
+// Local environment vars
+/*----------------------------------------------------*/
+return array(
+    'DB_NAME'       => 'themosisthemeexperiment',
+    'DB_USER'       => 'root',
+    'DB_PASSWORD'   => '',
+    'DB_HOST'       => 'localhost',
+    'WP_HOME'       => 'http://themosis-theme-experiment.dev/',
+    'WP_SITEURL'    => 'http://themosis-theme-experiment.dev/cms/'
+);
